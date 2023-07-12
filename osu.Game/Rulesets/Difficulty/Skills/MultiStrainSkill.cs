7a679def375e45a83b7ecc84e9bd307f723b3396
js1086 <js1086@student.le.ac.uk>
2023-07-12 14:59:58 +0100

    Improve structuring of new classes

--- osu.Game/Rulesets/Difficulty/Skills/MultiStrainSkill.cs
+++ osu.Game/Rulesets/Difficulty/Skills/MultiStrainSkill.cs
@@ -4,12 +4,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using osu.Framework.Utils;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
-using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Mods;
 
-namespace osu.Game.Rulesets.Osu.Difficulty.Skills
+namespace osu.Game.Rulesets.Difficulty.Skills
 {
     public abstract class MultiStrainSkill : Skill
     {
@@ -27,28 +25,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         /// The amount of concurrent strains this skill will be handling.
         /// </summary>
         protected virtual int ConcurrentStrainCount => 1;
-
-        /// <summary>
-        /// The default multiplier applied by <see cref="OsuStrainSkill"/> to the final difficulty value after all other calculations.
-        /// May be overridden via <see cref="DifficultyMultiplier"/>.
-        /// </summary>
-        public const double DEFAULT_DIFFICULTY_MULTIPLIER = 1.06;
-
-        /// <summary>
-        /// The number of sections with the highest strains, which the peak strain reductions will apply to.
-        /// This is done in order to decrease their impact on the overall difficulty of the map for this skill.
-        /// </summary>
-        protected virtual int ReducedSectionCount => 10;
-
-        /// <summary>
-        /// The baseline multiplier applied to the section with the biggest strain.
-        /// </summary>
-        protected virtual double ReducedStrainBaseline => 0.75;
-
-        /// <summary>
-        /// The final multiplier to be applied to the skill after all other calculations.
-        /// </summary>
-        protected virtual double DifficultyMultiplier => DEFAULT_DIFFICULTY_MULTIPLIER;
 
         private double[] currentSectionPeak; // We also keep track of the peak strain level in the current section.
 
@@ -103,7 +79,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         /// </summary>
         private void saveCurrentPeak(int offset)
         {
-            Console.WriteLine(currentSectionPeak[offset]);
             strainPeaks[offset].Add(currentSectionPeak[offset]);
         }
 
@@ -149,24 +124,15 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             // These sections will not contribute to the difficulty.
             var peaks = GetCurrentStrainPeaks(offset).Where(p => p > 0);
 
-            List<double> strains = peaks.OrderByDescending(d => d).ToList();
-
-            // We are reducing the highest strains first to account for extreme difficulty spikes
-            for (int i = 0; i < Math.Min(strains.Count, ReducedSectionCount); i++)
-            {
-                double scale = Math.Log10(Interpolation.Lerp(1, 10, Math.Clamp((float)i / ReducedSectionCount, 0, 1)));
-                strains[i] *= Interpolation.Lerp(ReducedStrainBaseline, 1.0, scale);
-            }
-
             // Difficulty is the weighted sum of the highest strains from every section.
             // We're sorting from highest to lowest strain.
-            foreach (double strain in strains.OrderByDescending(d => d))
+            foreach (double strain in peaks.OrderByDescending(d => d))
             {
                 difficulty += strain * weight;
                 weight *= DecayWeight;
             }
 
-            return difficulty * DifficultyMultiplier;
+            return difficulty;
         }
     }
 }
